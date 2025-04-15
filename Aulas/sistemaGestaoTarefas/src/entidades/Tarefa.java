f7e42128d385ac1bf7b1a8a9dbabb498c5ace426
Arthur Henrique <arthurbleckrep2@gmail.com>
2025-04-15 16:25:41 -0300

    Criando a classe "Tarefa" e o enum "StatusTarefa"

---
+++ Aulas/sistemaGestaoTarefas/src/entidades/Tarefa.java
@@ -0,0 +1,75 @@
+package entidades;
+
+import enums.StatusTarefa;
+
+import java.time.LocalDate;
+
+public class Tarefa {
+    private Integer id;
+    private String titulo;
+    private String descricao;
+    private LocalDate dataCriacao;
+    private LocalDate dataLimite;
+    private StatusTarefa status;
+
+    public Tarefa(Integer id, String titulo, String descricao, LocalDate dataCriacao, LocalDate dataLimite, StatusTarefa status) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.dataCriacao = dataCriacao;
+        this.dataLimite = dataLimite;
+        this.status = status;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDate dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public LocalDate getDataLimite() {
+        return dataLimite;
+    }
+
+    public void setDataLimite(LocalDate dataLimite) {
+        this.dataLimite = dataLimite;
+    }
+
+    public StatusTarefa getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusTarefa status) {
+        this.status = status;
+    }
+
+    public String toString() {
+        return "";
+    }
+}
