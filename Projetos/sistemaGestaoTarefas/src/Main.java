8b354546fad98b29d3801c42941d811f9c27e500
Arthur Henrique <arthurbleckrep2@gmail.com>
2025-04-17 16:02:10 -0300

    Refatorando o código

--- Projetos/sistemaGestaoTarefas/src/Main.java
+++ Projetos/sistemaGestaoTarefas/src/Main.java
@@ -16,73 +16,20 @@ public class Main {
         List<Tarefa> tarefas = new ArrayList<Tarefa>();
 
         do{
-            System.out.println("--------------------");
-            System.out.println("1 - Adicionar");
-            System.out.println("2 - Listar");
-            System.out.println("3 - Atualizar");
-            System.out.println("4 - Remover");
-            System.out.println("5 - Sair");
-            System.out.println("--------------------");
-
-            System.out.print("Opção: ");
-            controle = sc.nextInt();
+            controle = exibirMenu();
 
             switch(controle) {
                 case 1:
-                    System.out.print("Id: ");
-                    int id = sc.nextInt();
-                    sc.nextLine();
-                    System.out.print("Título: ");
-                    String titulo = sc.nextLine();
-                    System.out.print("Descrição: ");
-                    String descricao = sc.nextLine();
-                    LocalDate dataCriacao = LocalDate.now();
-
-                    DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-
-                    System.out.print("Data limite (dd/mm/aaaa): ");
-                    String dataLmt = sc.nextLine();
-                    LocalDate dataLimite = LocalDate.parse(dataLmt, fmt);
-
-                    System.out.print("Status: ");
-                    String status = sc.nextLine();
-
-                    tarefas.add(new Tarefa(id, titulo, descricao, dataCriacao, dataLimite, StatusTarefa.valueOf(status.toUpperCase())));
+                    tarefas.add(addTarefa());
                     break;
                 case 2:
-                    for(Tarefa tarefa : tarefas) {
-                        System.out.println(tarefas);
-                    }
+                    listarTarefas(tarefas);
                     break;
                 case 3:
-                    System.out.print("Id: ");
-                    id = sc.nextInt();
-                    sc.nextLine();
-
-                    for(Tarefa tarefa : tarefas) {
-                        if(tarefa.getId() == id) {
-                            System.out.printf("Título atual: %s%n", tarefa.getTitulo());
-                            System.out.print("Novo título: ");
-                            titulo = sc.nextLine();
-                            tarefa.setTitulo(titulo);
-
-                            System.out.printf("Descrição atual: %s%n", tarefa.getDescricao());
-                            System.out.print("Nova descrição: ");
-                            descricao = sc.nextLine();
-                            tarefa.setDescricao(titulo);
-
-                            System.out.println("Lista de tarefas atualizada!");
-                        } else {
-                            System.out.println("ID não encontrado!");
-                        }
-                    }
+                    atualizarTarefa(tarefas);
                     break;
                 case 4:
-                    System.out.print("Id: ");
-                    id = sc.nextInt();
-
-                    tarefas.removeIf(tarefa -> tarefa.getId() == id);
-                    System.out.println("Lista de tarefas atualizada!");
+                    removerTarefa(tarefas);
                     break;
                 case 5:
                     System.out.println("Finalizando...");
@@ -92,5 +39,87 @@ public class Main {
                     break;
             }
         }while(controle != 5);
+    }
+
+    public static int exibirMenu() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("--------------------");
+        System.out.println("1 - Adicionar");
+        System.out.println("2 - Listar");
+        System.out.println("3 - Atualizar");
+        System.out.println("4 - Remover");
+        System.out.println("5 - Sair");
+        System.out.println("--------------------");
+
+        System.out.print("Opção: ");
+
+        return sc.nextInt();
+    }
+
+    public static Tarefa addTarefa() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Id: ");
+        int id = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Título: ");
+        String titulo = sc.nextLine();
+        System.out.print("Descrição: ");
+        String descricao = sc.nextLine();
+        LocalDate dataCriacao = LocalDate.now();
+
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        System.out.print("Data limite (dd/mm/aaaa): ");
+        String dataLmt = sc.nextLine();
+        LocalDate dataLimite = LocalDate.parse(dataLmt, fmt);
+
+        System.out.print("Status: ");
+        String status = sc.nextLine();
+
+        return new Tarefa(id, titulo, descricao, dataCriacao, dataLimite, StatusTarefa.valueOf(status.toUpperCase()));
+    }
+
+    public static void listarTarefas(List<Tarefa> tarefas) {
+        for(Tarefa tarefa : tarefas) {
+            System.out.println(tarefas);
+        }
+    }
+
+    public static void atualizarTarefa(List<Tarefa> tarefas) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Id: ");
+        int id = sc.nextInt();
+        sc.nextLine();
+
+        for(Tarefa tarefa : tarefas) {
+            if(tarefa.getId() == id) {
+                System.out.printf("Título atual: %s%n", tarefa.getTitulo());
+                System.out.print("Novo título: ");
+                String titulo = sc.nextLine();
+                tarefa.setTitulo(titulo);
+
+                System.out.printf("Descrição atual: %s%n", tarefa.getDescricao());
+                System.out.print("Nova descrição: ");
+                String descricao = sc.nextLine();
+                tarefa.setDescricao(descricao);
+
+                System.out.println("Lista de tarefas atualizada!");
+            } else {
+                System.out.println("ID não encontrado!");
+            }
+        }
+    }
+
+    public static void removerTarefa(List<Tarefa> tarefas) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Id: ");
+        int id = sc.nextInt();
+
+        tarefas.removeIf(tarefa -> tarefa.getId() == id);
+        System.out.println("Lista de tarefas atualizada!");
     }
 }
